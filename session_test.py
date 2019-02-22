dd4cb9fbc1264173b30615022cb6dc65ec94cfa1
Paul <paul.leitner89@gmail.com>
2019-02-22 10:48:09 +0100

    pep 8

--- session_test.py
+++ session_test.py
@@ -1,13 +1,11 @@
 import requests
-import json
 import os
-import base64
 from requests.auth import HTTPBasicAuth
 from pprint import pprint
 
 
 url = 'https://rational-club.kontrast-dev.com/member/loginCC'
-#url = 'https://www.club-rational.com/member/loginCC'
+# url = 'https://www.club-rational.com/member/loginCC'
 token_url = 'https://www.connectedcooking.com/oauth/token'
 username, password, scope, client_id, client_secret = (os.environ.get(i) for i in
 ['stage_user', 'stage_password', 'stage_scope', 'stage_client_id', 'stage_client_secret'])

33f307b9903d80b2e3d42acc0205152d2f306912
Paul <paul.leitner89@gmail.com>
2019-03-05 14:26:49 +0100

    added switch for live/test refactored login function added URL parameter to login function

--- authentication.py
+++ authentication.py
@@ -7,17 +7,16 @@ from pprint import pprint
 
 
 logon_url_stage = 'https://rational-club.kontrast-dev.com/member/loginCC'
-logon_url_live = ''
+logon_url_live = 'https://wwww.club-rational.com/member/loginCC'
 token_url_stage = 'https://stage.connectedcooking.com/oauth/token'
 token_url_live = 'https://www.connectedcooking.com/oauth/token'
 
 
-def login_stage(*,
-                username: str = os.environ.get('stage_user'),
-                password: str = os.environ.get('stage_password'),
-                scope: str = os.environ.get('stage_scope'),
-                live: bool = False) -> dict:
-    url = logon_url_live if live is True else logon_url_stage
+def cc_login(*,
+             username: str = os.environ.get('stage_user'),
+             password: str = os.environ.get('stage_password'),
+             scope: str = os.environ.get('stage_scope'),
+                 url: str = 'https://rational-club.kontrast-dev.com/member/loginCC') -> dict:
     payload = {'username': username,
                'password': password}
     with requests.Session() as sess:
@@ -40,12 +39,11 @@ def login_stage(*,
 # needs boolean parameter - stage/live
 # needs check - token present & up to daten --> don't refresh!
 def get_bearer(*,
-               jwe: str = login_stage().get('jwe'),
+               jwe: str = cc_login().get('jwe'),
                client_id: str = os.environ.get('stage_client_id'),
                client_secret: str = os.environ.get('stage_client_secret'),
                scope: str = os.environ.get('stage_scope'),
-               live: bool = False) -> dict:
-    url = token_url_live if live is True else token_url_stage
+               url: str = 'https://stage.connectedcooking.com/oauth/token') -> dict:
     payload = {'jwe': jwe,
                'client_id': client_id,
                'client_secret': client_secret,
@@ -71,12 +69,43 @@ def get_bearer(*,
 
 
 if __name__ == '__main__':
-    print('testing function - login:')
-    pprint(login_stage().get('response'))
+    t_o_l = input('test live or stage? - l/s: ')
+    login_test, url, user, password = (None, None, None, None)
+    if t_o_l not in ['s', 'l']:
+        print('invalid choice')
+        quit()
+    elif t_o_l == 's':
+        print('testing function - login(stage):')
+        url = logon_url_stage
+        user = os.environ.get('stage_user')
+        password = os.environ.get('stage_password')
+    else:
+        print('testing function - login(live):')
+        url = logon_url_live
+        user = os.environ.get('live_user')
+        password = os.environ.get('live_password')
+    login_test = cc_login(url=url,
+                          username=user,
+                          password=password)
+    print('user', user)
+    print('password', password)
+    pprint(login_test)
     print('----------------------------')
     print('testing function - token')
-    bearer = get_bearer()
+    if t_o_l == 's':
+        bearer = get_bearer()
+    else:
+        bearer = get_bearer(url=token_url_live,
+                            client_id=os.environ.get('live_client_id'),
+                            client_secret=os.environ.get('live_client_secret'),
+                            jwe=login_test.get('jwe'))
+        pprint(bearer)
     # datetime needs to be converted to string to dump into JSON (datetime is not JSON serializable)
+    if bearer.get('response') in [400, 404, 401]:
+        print('authentication failed')
+        quit()
+    else:
+        pass
     bearer['valid_to'] = str(dt.datetime.now() + dt.timedelta(seconds=bearer.get('expires_in')))
     pprint(bearer.get('response'))
     print('token received is valid until: {expiration}'.

697ef667c9b28b3f3fe220433e704fc69de88750
Paul <paul.leitner89@gmail.com>
2019-03-13 09:07:21 +0100

    first draft of sub-class JWE token

--- token_class.py
+++ token_class.py
@@ -3,7 +3,7 @@ from time import sleep
 
 
 class Token:
-    def __init__(self, scope='live', created=datetime.now(), expires_in=100, valid=True):
+    def __init__(self, scope='HOFER_pleitner', created=datetime.now(), expires_in=86400, valid=True):
         self.scope = scope
         self.created = created
         self.expires_in = expires_in
@@ -14,23 +14,46 @@ class Token:
             self.valid = True
         else:
             self.valid = False
+        return self.valid
 
+
+class JWEToken(Token):
+    def __init__(self, site='live', token=None):
+        self.site = site
+        self.token = token
+        super().__init__()
+
+    def import_local(self,
+                     path=None):
+        if path is None:
+            path = self.site + path
+        return path
 
 # to do: inherited JWE/bearer class
 # add refresh function to both
 # add local-load to both
 
-if __name__ == '__main__':
-    test = Token('live', datetime.now(), 20, True)
 
-    for i in range(20):
-        test.check()
-        sleep(3.0)
-        print(i)
-        print('token scope: {scope}, created at:{created}, expires in {expires_in} seconds'.format(scope=test.scope,
-                                                                                                   created=test.created,
-                                                                                                   expires_in=test.expires_in))
-        if test.valid is True:
-            print('token is valid')
-        else:
-            print('token has expired!')
+if __name__ == '__main__':
+    jwe_test = JWEToken()
+    print(jwe_test.valid)
+    print(jwe_test.expires_in)
+    print(jwe_test.scope)
+    print(jwe_test.site)
+    print(jwe_test.check())
+    print(jwe_test.import_local())
+
+
+#    test = Token('HOFER_pleitner', datetime.now(), 20, True)
+
+#    for i in range(20):
+#        test.check()
+#        sleep(3.0)
+#        print(i)
+#        print('token scope: {scope}, created at:{created}, expires in {expires_in} seconds'.format(scope=test.scope,
+#                                                                                                   created=test.created,
+#                                                                                                   expires_in=test.expires_in))
+#        if test.valid is True:
+#            print('token is valid')
+#        else:
+#            print('token has expired!')

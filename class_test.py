3ddf2e79c47b625704c5cf97c272faee336f1bf5
Paul <paul.leitner89@gmail.com>
2019-03-15 10:35:22 +0100

    basic testing of class

---
+++ class_test.py
@@ -0,0 +1,20 @@
+from token_class import JWEToken, BearerToken
+from pprint import pprint
+
+
+jwe_test = JWEToken()
+print(jwe_test.request())
+print(jwe_test.export_local())
+print(jwe_test.import_local())
+
+bearer = BearerToken()
+print(bearer.request(jwe=jwe_test.token))
+print(bearer.export_local())
+print(bearer.import_local())
+
+
+#pprint(bearer.details)
+#print(type(bearer.valid_to))
+#print(bearer.created)
+#print(bearer.expires_in)
+#print(bearer.check())
